98098982edfcdecba8b426f4186a6a8111d77431
EthanGonzales <104255418+EthanGonzales@users.noreply.github.com>
2023-01-27 18:19:32 +0800

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,8 +1,8 @@
 function addUser()
 {
     Player1=document.getElementById("player1_name_input").value;
-    localStorage.setItem("player1", Player1);
+    localStorage.setItem("Player1", Player1);
     Player2=document.getElementById("player2_name_input").value;
-    localStorage.setItem("player2", Player2);
+    localStorage.setItem("Player2", Player2);
     window.location="quiz_page.html"
 }
